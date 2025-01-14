6cd692dc6dcff78cab2ce42cc316d00a60e02aef
Elias ZANOTTI <zanotti.elias@gmail.com>
2025-01-14 16:58:32 +0100

    add ejs

--- express/server.js
+++ express/server.js
@@ -4,10 +4,13 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-	res.sendFile(path.join(__dirname, "views", "index.html"));
+	res.render("index");
 });
 
 app.listen(PORT, () => {
