f388a04cfa02c63166f8ad7d1d449046ed1cfe38
Elias ZANOTTI <zanotti.elias@gmail.com>
2025-01-21 18:21:52 +0100

    add first working mail with brevo

--- express/routes.mjs
+++ express/routes.mjs
@@ -3,8 +3,13 @@ import User from "./models/User.mjs";
 import { emailValidation } from "./utils/emailValidation.mjs";
 import { usernameValidation } from "./utils/usernameValidation.mjs";
 import { passwordValidation } from "./utils/passwordValidation.mjs";
+import SibApiV3Sdk from "sib-api-v3-sdk";
 
 const router = express.Router();
+
+const defaultClient = SibApiV3Sdk.ApiClient.instance;
+const apiKey = defaultClient.authentications["api-key"];
+apiKey.apiKey = process.env.BREVO_API_KEY;
 
 router.get("/", (req, res) => {
 	res.render("index", { title: "Podium" });
@@ -62,7 +67,31 @@ router.post("/register", async (req, res) => {
 
 	try {
 		await newUser.save();
-		res.send("User added successfully !");
+
+		const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
+		const sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail();
+
+		sendSmtpEmail.sender = {
+			name: "Podium",
+			email: process.env.BREVO_SENDER_EMAIL,
+		};
+		sendSmtpEmail.to = [{ email: email, name: username }];
+		sendSmtpEmail.subject = "Vérification de votre email";
+		sendSmtpEmail.htmlContent = `
+			<html>
+				<body>
+					<h1>Veuillez cliquer sur ce lien pour vérifier votre email : 
+						<a href="http://localhost:3000/verify-email/${newUser._id}">Vérifier mon email</a>
+					</h1>
+					<p>Si vous n'avez pas créé de compte, veuillez ignorer ce message.</p>
+				</body>
+			</html>
+		`;
+
+		await apiInstance.sendTransacEmail(sendSmtpEmail);
+		res.send(
+			"User added successfully! Please check your email for verification."
+		);
 	} catch (error) {
 		res.status(500).render("register", {
 			id: "global",
