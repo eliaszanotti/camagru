e53c4bf6cca77af4498a7345da4f31c087d6ca88
Elias ZANOTTI <zanotti.elias@gmail.com>
2025-01-23 14:42:15 +0100

    add reset password

--- express/routes/authRoutes.mjs
+++ express/routes/authRoutes.mjs
@@ -1,14 +1,15 @@
 import express from "express";
 import User from "../models/User.mjs";
-import { passwordValidation } from "../utils/passwordValidation.mjs";
 import registerRoute from "./auth/register.mjs";
 import forgotPasswordRoute from "./auth/forgotPassword.mjs";
 import loginRoute from "./auth/login.mjs";
+import resetPasswordRoute from "./auth/resetPassword.mjs";
 
 const router = express.Router();
 router.use(registerRoute);
 router.use(forgotPasswordRoute);
 router.use(loginRoute);
+router.use(resetPasswordRoute);
 
 router.get("/check-email", (req, res) => {
 	res.render("checkEmail");
@@ -21,18 +22,6 @@ router.get("/logout", (req, res) => {
 
 router.get("/check-email-password", (req, res) => {
 	res.render("checkEmailPassword");
-});
-
-router.get("/reset-password/:token", async (req, res) => {
-	const { token } = req.params;
-	const user = await User.findOne({ resetPasswordToken: token });
-	if (!user) {
-		return res.status(404).send("Invalid or expired token.");
-	}
-	if (user.resetPasswordExpires < Date.now()) {
-		return res.status(404).send("Token expired.");
-	}
-	res.render("resetPassword", { token });
 });
 
 router.get("/verify-email/:token", async (req, res) => {
@@ -51,41 +40,6 @@ router.get("/verify-email/:token", async (req, res) => {
 		res.redirect("/auth/login");
 	} catch (error) {
 		res.status(500).send("Error verifying email.");
-	}
-});
-
-router.post("/reset-password/:token", async (req, res) => {
-	const { token } = req.params;
-	const { password } = req.body;
-
-	try {
-		const user = await User.findOne({ resetPasswordToken: token });
-		if (!user) {
-			return res.status(404).send("Invalid or expired token.");
-		}
-
-		if (user.resetPasswordExpires < Date.now()) {
-			return res.status(404).send("Token expired.");
-		}
-
-		if (!passwordValidation(password)) {
-			return res.render("resetPassword", {
-				id: "password",
-				message: "Password validation error: incorrect format",
-			});
-		}
-
-		user.password = password;
-		user.resetPasswordToken = undefined;
-		user.resetPasswordExpires = undefined;
-		await user.save();
-
-		res.redirect("/auth/login");
-	} catch (error) {
-		res.status(500).render("resetPassword", {
-			id: "global",
-			message: "Error resetting password",
-		});
 	}
 });
 
