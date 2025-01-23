664b0ce50a0e89ba8c4e05a2a8ebe379788dba9a
Elias ZANOTTI <zanotti.elias@gmail.com>
2025-01-23 15:41:31 +0100

    delete user from token (only add user id)

--- express/routes/index.mjs
+++ express/routes/index.mjs
@@ -1,6 +1,7 @@
 import express from "express";
 import authRoutes from "./authRoutes.mjs";
 import { authMiddleware } from "../middleware/authMiddleware.mjs";
+import User from "../models/User.mjs";
 
 const router = express.Router();
 
@@ -8,8 +9,13 @@ router.get("/", (req, res) => {
 	res.render("index", { title: "Podium" });
 });
 
-router.get("/profil", authMiddleware, (req, res) => {
-	res.render("profil", { user: req.user });
+router.get("/profil", authMiddleware, async (req, res) => {
+	const userId = req.user.id;
+	const user = await User.findById(userId);
+	if (!user) {
+		return res.status(404).render("404", { title: "404" });
+	}
+	res.render("profil", { user });
 });
 
 router.use("/auth", authRoutes);
--- express/routes/login.mjs
+++ express/routes/login.mjs
@@ -30,7 +30,7 @@ router.post("/login", async (req, res) => {
 	}
 
 	const token = jwt.sign(
-		{ id: user._id, username: user.username, email: user.email },
+		{ id: user._id },
 		process.env.JWT_SECRET,
 		{
 			expiresIn: "1h",
