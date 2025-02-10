c901fc56f59d1167f572ccd6dfa1f8227d9f6a11
Elias ZANOTTI <zanotti.elias@gmail.com>
2025-02-10 16:14:16 +0100

    add fetch multiple posts

--- express/routes/postRoutes.mjs
+++ express/routes/postRoutes.mjs
@@ -9,19 +9,23 @@ const router = express.Router();
 
 const storage = multer.diskStorage({
 	destination: function (req, file, cb) {
-		cb(null, 'uploads/');
+		cb(null, "uploads/");
 	},
 	filename: function (req, file, cb) {
-		const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+		const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
 		cb(null, uniqueSuffix + path.extname(file.originalname));
-	}
+	},
 });
 
 const upload = multer({ storage: storage });
 
 router.get("/get-dual", async (req, res) => {
 	try {
-		const posts = await Post.aggregate([{ $sample: { size: 2 } }]);
+		const posts = [];
+		for (let i = 0; i < req.query.count; i++) {
+			const post = await Post.aggregate([{ $sample: { size: 2 } }]);
+			posts.push(post);
+		}
 		res.status(200).json(posts);
 	} catch (error) {
 		res.status(500).json(errors.GETTING_POSTS.message);
@@ -36,31 +40,38 @@ router.get("/webcam", authMiddleware, (req, res) => {
 	res.render("postWebcam");
 });
 
-router.post("/publish", authMiddleware, upload.single("imageFile"), async (req, res) => {
-	try {
-		const userId = req.user.id;
-		const imageFile = req.file;
+router.post(
+	"/publish",
+	authMiddleware,
+	upload.single("imageFile"),
+	async (req, res) => {
+		try {
+			const userId = req.user.id;
+			const imageFile = req.file;
 
-		if (!imageFile) {
-			return res.status(400).json({ message: "Image file is required" });
+			if (!imageFile) {
+				return res.status(400).json({ message: "Image file is required" });
+			}
+
+			const imageUrl = `/uploads/${imageFile.filename}`;
+
+			const newPost = new Post({
+				userId: userId,
+				imageUrl: imageUrl,
+				createdAt: new Date(),
+				votes: 0,
+			});
+
+			await newPost.save();
+
+			res
+				.status(201)
+				.json({ message: "Post created successfully", post: newPost });
+		} catch (error) {
+			console.error(error);
+			res.status(500).json({ message: "Error creating post" });
 		}
-
-		const imageUrl = `/uploads/${imageFile.filename}`;
-
-		const newPost = new Post({
-			userId: userId,
-			imageUrl: imageUrl,
-			createdAt: new Date(),
-			votes: 0,
-		});
-
-		await newPost.save();
-
-		res.status(201).json({ message: "Post created successfully", post: newPost });
-	} catch (error) {
-		console.error(error);
-		res.status(500).json({ message: "Error creating post" });
 	}
-});
+);
 
 export default router;
