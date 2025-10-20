00ce3d162563b103c3edb29c3bd2265a2a74ec0b
Elias ZANOTTI <zanotti.elias@gmail.com>
2025-10-20 15:51:18 +0200

    delete logs

--- php/assets/js/browse-component.js
+++ php/assets/js/browse-component.js
@@ -1,8 +1,6 @@
 class BrowseComponent {
 	constructor() {
-		console.log("All file inputs on page:", document.querySelectorAll('input[type="file"]'));
 		this.fileInput = document.getElementById("fileInput");
-		console.log("BrowseComponent: fileInput found:", !!this.fileInput);
 		this.init();
 	}
 
@@ -11,34 +9,24 @@ class BrowseComponent {
 	}
 
 	handleFileSelect(e) {
-		console.log("handleFileSelect called");
 		const file = e.target.files[0];
-		console.log("File selected:", file);
 		if (file) {
 			const reader = new FileReader();
 			reader.onload = (e) => {
-				console.log("FileReader loaded, image data length:", e.target.result.length);
 				this.updatePreview(e.target.result);
 			};
 			reader.readAsDataURL(file);
 		}
 	}
 
-
 	updatePreview(imageData) {
-		console.log("updatePreview called, window.updatePreview exists:", !!window.updatePreview);
 		if (window.updatePreview) {
-			console.log("Calling window.updatePreview with image data");
 			window.updatePreview(imageData, "file");
-		} else {
-			console.error("window.updatePreview is not defined!");
 		}
 	}
 
 	setupEventListeners() {
-		console.log("Setting up event listeners");
 		this.fileInput.addEventListener("change", (e) => {
-			console.log("File input change event triggered");
 			this.handleFileSelect(e);
 		});
 	}
