8ab7a26016795f07b6e0a2c4393cb2c87c09bc35
Elias ZANOTTI <zanotti.elias@gmail.com>
2025-12-02 14:28:12 +0100

    fix and add responsive on header

--- php/includes/header.php
+++ php/includes/header.php
@@ -26,25 +26,29 @@ $currentPage = basename($_SERVER['PHP_SELF'], '.php');
         </div>
     <?php endif; ?>
 
-    <div class="w-full px-8">
-        <header class="navbar mx-auto max-w-6xl">
+    <div class="w-full">
+        <header class="navbar mx-auto max-w-6xl px-4">
             <div class="navbar-start">
-                <a href="index.php" class="btn btn-ghost">Camagru</a>
-            </div>
-            <div class="navbar-center gap-2">
-                <a href="index.php" class="<?php echo $currentPage === 'index' ? 'active' : ''; ?> btn btn-ghost">Home</a>
-                <?php if ($isLoggedIn): ?>
-                    <a href="create.php" class="<?php echo $currentPage === 'create' ? 'active' : ''; ?> btn btn-ghost">Create</a>
-                <?php endif; ?>
+                <a href="index.php" class="<?php echo $currentPage === 'index' ? 'active' : ''; ?> btn md:btn-md btn-sm btn-ghost">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="size-6 lucide lucide-house-icon lucide-house">
+                        <path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8" />
+                        <path d="M3 10a2 2 0 0 1 .709-1.528l7-6a2 2 0 0 1 2.582 0l7 6A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
+                    </svg></a>
             </div>
             <div class="navbar-end gap-2">
                 <?php if ($isLoggedIn): ?>
-                    <a href="gallery.php" class="<?php echo $currentPage === 'gallery' ? 'active' : ''; ?> btn btn-ghost">Gallery</a>
-                    <a href="profile.php" class="btn btn-ghost">Profile</a>
-                    <a href="logout.php" class="btn btn-ghost">Logout</a>
+                    <a href="gallery.php" class="<?php echo $currentPage === 'gallery' ? 'active' : ''; ?> btn md:btn-md btn-sm btn-ghost">Gallery</a>
+                    <a href="profile.php" class="btn md:btn-md btn-sm btn-ghost">Profile</a>
+                    <a href="logout.php" class="btn md:btn-md btn-sm btn-ghost">Logout</a>
+                    <a href="create.php" class="<?php echo $currentPage === 'create' ? 'active' : ''; ?> btn md:btn-md btn-sm btn-square btn-primary">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="size-6 lucide lucide-plus-icon lucide-plus">
+                            <path d="M5 12h14" />
+                            <path d="M12 5v14" />
+                        </svg>
+                    </a>
                 <?php else: ?>
-                    <a href="login.php" class="btn btn-ghost">Login</a>
-                    <a href="register.php" class="btn btn-primary">Register</a>
+                    <a href="login.php" class="btn md:btn-md btn-sm btn-ghost">Login</a>
+                    <a href="register.php" class="btn md:btn-md btn-sm btn-primary">Register</a>
                 <?php endif; ?>
             </div>
         </header>
